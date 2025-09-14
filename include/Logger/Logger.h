abacd7665de5c871b42b48c5e96d7dae421a3b84
egbog <3422424+egbog@users.noreply.github.com>
2025-09-13 21:27:44 -0700

    logger shutdown and unlock while cout. take ostringstream param as value instead of ref

--- include/Logger/Logger.h
+++ include/Logger/Logger.h
@@ -8,17 +8,20 @@
 class Logger
 {
 public:
-  Logger()                            = default;
-  ~Logger()                           = default;
+  Logger() = default;
+  ~Logger();
   Logger& operator=(Logger& t_other)  = delete;
   Logger& operator=(Logger&& t_other) = delete;
   Logger(Logger& t_other)             = delete;
   Logger(Logger&& t_other)            = delete;
 
-  [[noreturn]] void LoggerWorkerThread();
-  void              ThreadSafeLogMessage(std::ostringstream& t_entry);
+  void LoggerWorkerThread();
+  void ThreadSafeLogMessage(std::ostringstream t_entry);
 
 private:
-  std::queue<LogEntry> m_logQueue;
-  std::mutex           m_waitLogMutex;
+  void                    Shutdown();
+  std::queue<LogEntry>    m_logQueue;
+  std::mutex              m_waitLogMutex;
+  std::condition_variable m_cv;
+  bool                    m_shutdown = false;
 };
--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -1,27 +1,40 @@
-#include <Logger/Logger.h>
-
 #include <iostream>
 
+#include <Logger/Logger.h>
+
+Logger::~Logger() {
+  Shutdown();
+}
+
 void Logger::LoggerWorkerThread() {
+  std::cout << "Logger worker thread dispatched to thread: " << std::this_thread::get_id() << '\n';
   while (true) {
     std::unique_lock lock(m_waitLogMutex);
-    if (!m_logQueue.empty()) {
-      while (!m_logQueue.empty()) {
-        auto& [message] = m_logQueue.front();
-        std::cout << message.str();
-        m_logQueue.pop();
-      }
+
+    // wait until there's something in the log queue
+    m_cv.wait(lock, [this] { return !m_logQueue.empty() || m_shutdown; });
+
+    if (m_shutdown && m_logQueue.empty()) {
+      break;
+    }
+
+    while (!m_logQueue.empty()) {
+      auto [message] = std::move(m_logQueue.front());
+      m_logQueue.pop();
 
       lock.unlock();
-    }
-    else {
-      lock.unlock();
-      std::this_thread::sleep_for(std::chrono::nanoseconds(10));
+      std::cout << message.str();
+      lock.lock();
     }
   }
 }
 
-void Logger::ThreadSafeLogMessage(std::ostringstream& t_entry) {
+void Logger::ThreadSafeLogMessage(std::ostringstream t_entry) {
   std::lock_guard lock(m_waitLogMutex);
   m_logQueue.emplace(std::move(t_entry));
+  m_cv.notify_one();
+}
+
+void Logger::Shutdown() {
+  m_shutdown = true;
 }
