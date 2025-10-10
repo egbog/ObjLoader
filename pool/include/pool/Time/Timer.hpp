9ef6c81ea6a7eb775a8a25c857b7c47acaa08196
egbog <3422424+egbog@users.noreply.github.com>
2025-10-09 17:54:21 -0700

    unnecessary template

--- pool/include/pool/Time/Timer.hpp
+++ pool/include/pool/Time/Timer.hpp
@@ -7,19 +7,11 @@ public:
   using Clock     = std::chrono::high_resolution_clock;
   using TimePoint = Clock::time_point;
 
-  Timer() { Reset(); }
-
+  Timer();
   // Reset the timer to "now"
-  void Reset() {
-    m_start = Clock::now();
-  }
-
-  // Generic elapsed time in any chrono duration type
-  template <typename DurationT = std::milli>
-  [[nodiscard]] std::chrono::duration<double, DurationT> Elapsed() const;
+  void                                                    Reset();
+  [[nodiscard]] std::chrono::duration<double, std::milli> Elapsed() const;
 
 private:
   TimePoint m_start;
 };
-
-#include "Time/Timer.inl"
--- pool/include/pool/Time/Timer.inl
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-template <typename DurationT>
-[[nodiscard]] std::chrono::duration<double, DurationT> Timer::Elapsed() const {
-  return std::chrono::duration<double, DurationT>(Clock::now() - m_start);
-}
